28be7aa2cfb90f7b07142a11148d839ba5d4b24c
leeleeleeleejun <wnstjr6293@naver.com>
2023-08-10 20:19:24 +0900

    초기 환경설정

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -1,22 +1,12 @@
-import {useEffect, useState} from 'react'
-import './App.css'
-import axios from "axios";
-
 function App() {
-  const [hello, setHello] = useState('');
-    console.log(hello);
-  useEffect(() => {
-      axios.get('/api/hello')
-          .then(response => setHello(response.data))
-          .catch(error => console.log(error))
+  // useEffect(() => {
+  //   fetch("/api/hello")
+  //     .then((response) => response.json())
+  //     .then((response) => setHello(response.data))
+  //     .catch((error) => console.log(error));
+  // }, []);
 
-  }, []);
-
-  return (
-    <>
-        백엔드에서 가져온 데이터입니다 : { hello }
-    </>
-  );
+  return <>백엔드에서 가져온 데이터입니다 :</>;
 }
 
 export default App;
