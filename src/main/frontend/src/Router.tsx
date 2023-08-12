641b75326f9646aa89a83770c8a8caeedb45d160
leeleeleeleejun <wnstjr6293@naver.com>
2023-08-12 14:16:37 +0900

    feat: 로그인 UI 완료

--- src/main/frontend/src/Router.tsx
+++ src/main/frontend/src/Router.tsx
@@ -2,14 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Layout from "./components/common/Layout";
 import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path={"/"} element={<HomePage />}></Route>
-          <Route path={"/signup"} element={<SignUpPage />}></Route>
+          <Route path={"/"} element={<HomePage />} />
+          <Route path={"/signup"} element={<SignUpPage />} />
+          <Route path={"/login"} element={<LoginPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
