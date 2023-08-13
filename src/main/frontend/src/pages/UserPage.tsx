af08eb4d4afe1ade5b46a92d645ccb209a7133c5
leeleeleeleejun <wnstjr6293@naver.com>
2023-08-13 16:15:18 +0900

    feat: user정보 수정 페이지 유효성평가 추가

--- src/main/frontend/src/pages/UserPage.tsx
+++ src/main/frontend/src/pages/UserPage.tsx
@@ -2,38 +2,68 @@ import FormFiled from "@/components/common/FormField";
 import { SpaceLine } from "@/components/user/Line";
 import EditProfileImage from "@/components/user/EditProfileImage";
 import FormButton from "@/components/common/FormButton";
+import { useCallback, useState } from "react";
+import { userKey, editUserInfo } from "@/types";
+import { EditUserInfoValidate } from "@/utils/validate";
+import ValidErrorMessage from "@/components/common/ValidErrorMessage";
 
 const UserPage = () => {
+  const [editUserInfo, setEditUserInfo] = useState<editUserInfo>({
+    nickname: "",
+    password: "",
+    confirmPassword: "",
+    phone: "",
+  });
+
+  const [validateError, serValidateError] = useState("");
+
+  const { nickname, password, confirmPassword, phone } = editUserInfo;
+
+  const setEditUserInfoFunc = useCallback((value: string, key?: userKey) => {
+    if (!key) return;
+    setEditUserInfo((prev) => ({ ...prev, [key]: value }));
+  }, []);
+
   return (
     <>
       <EditProfileImage />
       <SpaceLine />
       <FormFiled
-        label="닉네임"
         name="nickname"
         type="text"
         placeholder="2글자 이상"
+        value={nickname}
+        onChange={setEditUserInfoFunc}
       />
       <FormFiled
-        label="비밀번호"
         name="password"
         type="password"
         placeholder="특수문자, 문자, 숫자 포함 8~15자"
+        value={password}
+        onChange={setEditUserInfoFunc}
       />
       <FormFiled
-        label="비밀번호 확인"
-        name="passwordConfirm"
+        name="confirmPassword"
         type="text"
         placeholder="특수문자, 문자, 숫자 포함 8~15자"
+        value={confirmPassword}
+        onChange={setEditUserInfoFunc}
       />
       <FormFiled
-        label="전화번호"
-        name="phoneNumber"
+        name="phone"
         type="tel"
         placeholder="01011112222"
+        value={phone}
+        onChange={setEditUserInfoFunc}
       />
-
-      <FormButton>수정하기</FormButton>
+      <ValidErrorMessage>{validateError}</ValidErrorMessage>
+      <FormButton
+        onClick={() => {
+          EditUserInfoValidate(editUserInfo, serValidateError);
+        }}
+      >
+        수정하기
+      </FormButton>
     </>
   );
 };
