8efd20c9252c25d51ce853886973220cf323e420
leeleeleeleejun <wnstjr6293@naver.com>
2023-08-21 15:10:40 +0900

    feat: 회원가입 완료 시 router 지정

--- src/main/frontend/src/api/user.ts
+++ src/main/frontend/src/api/user.ts
@@ -1,12 +1,22 @@
 import { API_PATH } from "@/constants/index";
 import { userInfo, loginInfo } from "@/types";
 import callApi from "@/utils/callApi";
+import { NavigateFunction } from "react-router-dom";
 
-export const postUser = async (data: userInfo) => {
-  await callApi("POST", API_PATH.USER_SIGNUP, JSON.stringify(data));
+export const postUser = async (data: userInfo, navigate: NavigateFunction) => {
+  const response = await callApi(
+    "POST",
+    API_PATH.USER_SIGNUP,
+    JSON.stringify(data)
+  );
+
+  if (response.ok) {
+    alert("회원가입이 완료되었습니다.");
+    navigate("/login");
+  }
 };
 
-export const login = async (data: loginInfo) => {
+export const login = async (data: loginInfo, navigate: NavigateFunction) => {
   const response = await callApi(
     "POST",
     API_PATH.USER_LOGIN,
@@ -16,7 +26,7 @@ export const login = async (data: loginInfo) => {
     const result = await response.json();
     localStorage.setItem("token", result.accessToken);
     alert("로그인이 완료되었습니다.");
-    data.navigate("/");
+    navigate("/");
   } else {
     alert("이메일 또는 비밀번호가 일치하지 않습니다.");
   }
